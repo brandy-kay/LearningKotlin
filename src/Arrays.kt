97a21b2881cf9e78c0346462b28bf879ba7e4979
brandy-kay <brandyariana99@gmail.com>
2021-09-17 11:19:21 +0300

    arrays

---
+++ src/Arrays.kt
@@ -0,0 +1,5 @@
+fun main(args:Array<String>) {
+    val evenNumber= arrayOf(2,4,5,66); //creating an array which is of type Int
+    val fiveFives = Array(5,{5}) //{5}-lambdas in kotlin
+    val vowel= arrayOf("a","e","i","o","u") //creating array of type String
+}
--- src/Nullability.kt
+++ src/Nullability.kt
@@ -1,19 +1,55 @@
 fun main() {
-    var age : Int? = 21  // declare variable of null type
+    var age: Int? = 21  // declare variable of null type
     println(age)
 
     //println(age+1)  //raises an error
     //to remove null
-    val afterbirthday = age!!+1 //!! not-null assertion operator
+
+    //!! not-null assertion operator
+    val afterbirthday = age!! + 1
     println(afterbirthday)
-    val ageC=age?.toChar() //?. safe call
+
+    //?. safe call allows the use of various methods
+    val ageC = age?.toChar()
     println(ageC)
 
+    //smart casts
     val userInput = readLine() //allow user to enter inputs
     if (userInput != null)
-        println(userInput.toInt()-5) //performs arithmetic operations when enters a string causes an error
+        println(userInput.toInt() - 5) //performs arithmetic operations when enters a string causes an error
 
-//assignment
+//let() function is used with the help of safe call ?.
+    val authorName: String? = "Daphine"
+    authorName?.let {
+        var nonNullableAuthor = authorName
+        var nameLength = authorName.length
+
+        println(nonNullableAuthor)
+        println(nameLength)
+    }
+
+    //Elvis operator ?:
+    var num: Int? = null
+    var mustHaveNum = num ?: 0 //using elvis operator mustHaveNum will either cointain num value if not null or 0 if num is null.
+    println(mustHaveNum)
+
+    //example
+    println("Please enter your age")
+    val ageNum = readLine() ?. toInt()
+
+   if (ageNum!=null) //using smart cast
+   {
+       if (ageNum>0 && ageNum <18){
+           println("You are not an adult yet")
+       }
+       else if(ageNum >=18 && ageNum <=65){
+           println("You are an adult")
+       }
+       else{
+           println("You  are to old")
+       }
+   }
+
 
 
 }
